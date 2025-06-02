dc02216c6c581496feba2293f1018581041f77e5
salsal <to.salsal.pro@gmail.com>
2025-06-03 00:16:51 +0330

    Update page.tsx
    121212

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly. 
-            /////////////////////////////////
+            ///////////////////////////////// 1 2 3 4
           </li>
         </ol>
 
